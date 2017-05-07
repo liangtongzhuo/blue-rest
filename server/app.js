2c9436679fc2477bf2bdf6c0c77eb4a08fcd1a60
梁亚宾 <liangyabin@wifi.local>
2017-05-07 17:59:58 +0800

    update

--- server/app.js
+++ server/app.js
@@ -5,15 +5,10 @@ require('./init/mongoose');
 //加载配置
 const config = require('./config.js');
 const express = require('express');
-const router = require('./routes/index.js');
+const mini_rest = require('./routes/index.js');
 
 const app = express();
 
-//解析 body
-// app.use(bodyParser.urlencoded({
-    // extended: true
-// }));
-// app.use(bodyParser.json());
 //rest Api
 app.use('/', router);
 
